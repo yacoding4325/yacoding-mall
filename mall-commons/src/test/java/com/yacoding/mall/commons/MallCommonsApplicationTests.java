38dbe20218bb05303922bcdb4a160faeeeb49320
yxym0203 <1500081802@qq.com>
2022-12-27 22:22:17 +0800

    通用的模块

---
+++ mall-commons/src/test/java/com/yacoding/mall/commons/MallCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yacoding.mall.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
