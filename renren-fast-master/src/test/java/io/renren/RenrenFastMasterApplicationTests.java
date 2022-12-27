d22bd1967b99f296b553dd993f03e7773a2b37ca
yxym0203 <1500081802@qq.com>
2022-12-27 23:06:32 +0800

    后台管理系统搭建--后台项目

---
+++ renren-fast-master/src/test/java/io/renren/RenrenFastMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.renren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenrenFastMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
